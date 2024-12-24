788bb00fa471ee72d6d32513b9a603855ebe60df
facundozapata2 <facundozapata2@gmail.com>
2024-12-24 17:41:22 -0300

    footer component added succesfully

---
+++ assets/components/footer-component/footer.js
@@ -0,0 +1,33 @@
+// html custom element
+
+class Mainfooter extends HTMLElement {
+    constructor() {
+    super();
+}
+
+    async connectedCallback() {
+    try {
+      // html
+    const response = await fetch('./assets/components/footer-component/footer.html');
+    if (!response.ok) {
+        throw new Error(`Error al cargar el HTML: ${response.statusText}`);
+    }
+        this.innerHTML = await response.text();
+
+      // css
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = './assets/components/footer-component/footer.css';
+    document.head.appendChild(link);
+      // js
+    const script = document.createElement("script");
+    script.src = './assets/js/footer.js';
+    script.defer = true;
+    document.body.appendChild(script);
+
+        } catch (error) {
+            console.error('Error al cargar el componente Footer:', error);
+        }
+    }
+}
+customElements.define('main-footer', Mainfooter);
